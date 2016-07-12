0fac8351c881351e7dac135af4401afd1117bd0a
kunalrai <ikunalrai@gmail.com>
2016-07-12 23:46:43 +0530

    nodemon

---
+++ public/assets/js/routes.js
@@ -0,0 +1,4 @@
+var foundationRoutes = [
+    {"name":"settings","url":"/settings","animationIn":"slideFromLeft",
+    "animationOut":"slideFromLeft","path":"templates/settings.html"},
+    {"name":"home","url":"/","path":"templates/home.html"}];
